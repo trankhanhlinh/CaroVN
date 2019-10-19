b5b59328276369f4e21106f41ca3d323676af2d6
Trần Khánh Linh <trankhanhlinh98@gmail.com>
2019-10-20 01:47:49 +0700

    add react-router-dom and react-boostrap

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Login from './login';
+import Register from './register';
 import Game from '../containers/game';
 
-const App = () => (
-  <div>
-    <Game />
-  </div>
-);
-
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/game">
+            <Game />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import rootReducer from './reducers';
 import './index.css';
 import App from './components/app';
